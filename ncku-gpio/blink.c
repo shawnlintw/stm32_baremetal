a2412a0d3b6becbdb77c8662b130a797c7b281f6
shawnlin <edwin2619@gmail.com>
2020-03-04 20:01:00 +0800

    create a repository

---
+++ ncku-gpio/blink.c
@@ -0,0 +1,24 @@
+#define GPIOG_MODER ( *((volatile unsigned long *) (0x40021800+ 0x0) ) )
+#define GPIOG_OSPEEDR (*((volatile unsigned long *) (0x40021800+ 0x8) ) )
+#define GPIOG_BSRR	( *((volatile unsigned long *) (0x40021800+ 0x18)) )
+#define RCC_AHB1ENR	( *((volatile unsigned long *) (0x40023800+ 0x30)) )
+
+
+asm(".word 0x20001000");
+asm(".word main");
+
+int main(){
+
+	unsigned int c=0;
+
+	RCC_AHB1ENR = (1<<6); 		/* GPIOGEN =1 */
+	GPIOG_MODER = 0x14000000;   // set 13 and 14 be output mode.
+
+	while(1){
+		GPIOG_BSRR=(3<<13); 	/* ON  */
+		for (c=0; c<500000; c++);
+		GPIOG_BSRR= (3<<29); 	/* OFF */
+		for (c=0; c<500000; c++);
+	}
+}
+
